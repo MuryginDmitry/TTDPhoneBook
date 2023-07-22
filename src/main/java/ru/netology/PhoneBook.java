edc6791b59b555b27b08402d7f5225d1c98e7707
DmitryM <tihiydon@list.ru>
2023-07-22 16:41:39 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,20 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts;
+
+    public PhoneBook() {
+        this.contacts = new HashMap<>();
+    }
+
     public int add(String name, String phoneNumber) {
-        return 0;
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, phoneNumber);
+            return contacts.size();
+        }
+        return contacts.size();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -10,7 +10,16 @@ public class PhoneBookTest {
         int initialSize = phoneBook.add("John Doe", "1234567890");
         int newSize = phoneBook.add("Jane Smith", "9876543210");
 
-        Assertions.assertEquals(0, initialSize);
-        Assertions.assertEquals(1, newSize);
+        Assertions.assertEquals(1, initialSize);
+        Assertions.assertEquals(2, newSize);
+    }
+
+    @Test
+    public void testAddDuplicateContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        int size = phoneBook.add("John Doe", "1234567890");
+        size = phoneBook.add("John Doe", "9876543210");
+
+        Assertions.assertEquals(1, size);
     }
 }
