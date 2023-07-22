c01df3a106a72e220bd291a6c7cf95cd6bccac4a
DmitryM <tihiydon@list.ru>
2023-07-22 16:31:59 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,16 @@
+import junit.framework.TestCase;
+import ru.netology.PhoneBook;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class PhoneBookTest {
+    @Test
+    public void testAddContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        int initialSize = phoneBook.add("John Doe", "1234567890");
+        int newSize = phoneBook.add("Jane Smith", "9876543210");
+
+        Assertions.assertEquals(0, initialSize);
+        Assertions.assertEquals(1, newSize);
+    }
 }
